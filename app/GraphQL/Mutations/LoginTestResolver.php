e7cb2694bf249f707bf2a3afa1f02a2765ae68c0
vagrant <vagrant@homestead>
2023-01-28 13:03:24 +0000

    Authentication in Lighthouse

---
+++ app/GraphQL/Mutations/LoginTestResolver.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+final class LoginTestResolver
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        $user = User::where('email', $args['email'])->first();
+
+        if (! $user || ! Hash::check($args['password'], $user->password)) {
+
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token =$user->createToken($args['device_name'])->plainTextToken;
+
+        return [
+            'token' => $token,
+            'user' => $user->only('id', 'name', 'username', 'email', 'avatar')
+        ];
+    }
+}
